604e6a154dd17d3d2289a2ceb9b712f3cb2b4c64
Sw0xy <58565946+Sw0xy@users.noreply.github.com>
2023-07-08 00:16:30 +0300

    Update auth.ts

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -54,10 +54,6 @@ export const authOptions: NextAuthOptions = {
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
-    /*  GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-    }), */
   ],
 };
 
