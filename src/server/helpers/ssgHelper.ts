cc715431a09d9e02e02adb14c94b9726127495fd
Sw0xy <aliemreali5@gmail.com>
2023-07-10 03:18:42 +0300

    remove firebase

--- src/server/helpers/ssgHelper.ts
+++ src/server/helpers/ssgHelper.ts
@@ -2,9 +2,10 @@ import { createServerSideHelpers } from "@trpc/react-query/server";
 import superjson from "superjson";
 import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db";
+import { s3 } from "~/server/aws/s3";
 export const generateSSGHelper = () =>
   createServerSideHelpers({
     router: appRouter,
-    ctx: { prisma, session: null },
+    ctx: { prisma, session: null, s3 },
     transformer: superjson, // optional - adds superjson serialization
   });
